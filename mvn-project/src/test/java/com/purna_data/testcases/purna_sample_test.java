499f3d8b427abf3571abaecbba34978a8b9d7672
abhishek <abhishek@DESKTOP-9NRRN6K>
2022-10-13 14:15:49 +0530

    updating the sample test file

--- mvn-project/src/test/java/com/purna_data/testcases/purna_sample_test.java
+++ mvn-project/src/test/java/com/purna_data/testcases/purna_sample_test.java
@@ -1,5 +1,13 @@
 package com.purna_data.testcases;
 
-public class purna_sample_test {
+import org.junit.Test;
 
+import com.purna_data.libraries.BaseClass;
+
+public class purna_sample_test extends BaseClass{
+	
+	@Test
+	public void sample() {
+		System.out.println("hello");
+	}
 }

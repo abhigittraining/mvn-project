33761f7d42932082ae78dd26cd91baf9bb527349
abhigittraining <114486110+abhigittraining@users.noreply.github.com>
2022-10-13 14:26:07 +0530

    Create purna_sample_selenium_test.java

---
+++ mvn-project/src/test/java/com/purna_data/testcases/purna_sample_selenium_test.java
@@ -0,0 +1,18 @@
+package com.purna_data.testcases;
+
+import org.testng.annotations.Test;
+
+import com.purna_data.libraries.BaseClass;
+
+public class purna_sample_selenium_test extends BaseClass{
+	
+	@Test
+	public void testng() {
+		System.out.println("testng");
+	}
+	
+	@Test
+	public void selenium() {
+		System.out.println("selenium");
+	}
+}
